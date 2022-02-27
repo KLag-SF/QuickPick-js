9d827856275e764b240902bc36251624335bdb8e
KLag-SF <k.lagopus.sf19@gmail.com>
2022-02-27 18:37:45 +0900

    Add color profile

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,24 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes:{
+            light:{
+                primary: colors.green.darken1,
+                // Color of each blackets
+                blacket1: colors.shades.white,
+                blacket2: colors.shades.black,
+                blacket3: colors.red.darken2,
+                blacket4: colors.blue.darken2,
+                blacket5: colors.yellow.accent2,
+                blacket6: colors.lightGreen.accent3,
+                blacket7: colors.yellow.darken4,
+                blacket8: colors.pink.lighten4,
+            }
+        }
+    }
 });
