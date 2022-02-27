28b2dfce4acac82334137d09e20a70744ba3d684
KLag-SF <k.lagopus.sf19@gmail.com>
2022-02-27 20:54:24 +0900

    Revert "Add Vuex library"
    This reverts commit 2c98cf94958727487970a68ab206aeb4b4b1a497.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-Vue.use(Vuex)
 
 new Vue({
   vuetify,
