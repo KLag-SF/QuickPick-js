d29c918347e30dab3a132dbd2e3cad7d00ff923c
KLag-SF <k.lagopus.sf19@gmail.com>
2022-07-31 12:57:06 +0900

    gh-pages deploy settings

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  PublicPath: process.env.NODE_ENV === 'production'
+            ? '/quickpick-js/'
+            : '/',
+
 })
