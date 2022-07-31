25b8d168d55ed6599f2e7b130d1839a9f831acac
KLag-SF <k.lagopus.sf19@gmail.com>
2022-07-31 14:07:21 +0900

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,32 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-    <h3>QuickPick.js</h3>
-    </v-app-bar>
-
-    <v-main>
-      <selector-component @selected="setPickedNumber"/>
-      <result-component :picked="picked"/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-import SelectorComponent from './components/SelectorComponent';
-import ResultComponent from "@/components/ResultComponent";
+#nav {
+  padding: 30px;
 
-export default {
-  name: 'App',
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  components: {
-    SelectorComponent,
-    ResultComponent,
-  },
-
-  data: () => ({
-    picked: [],
-  }),
-
-  methods:{
-    setPickedNumber: function(picked){
-      this.picked = picked
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
-};
-</script>
+}
+</style>
